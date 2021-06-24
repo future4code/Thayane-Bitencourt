e5e208be83b600fa2b8c7edcd95e629130e1f237
Thayane-Bitencourt <thayanebitencourt.ssa@gmail.com>
2021-06-24 17:16:19 -0300

    aula 10

---
+++ semana2/loops/index.js
@@ -0,0 +1,12 @@
+//**EXERCICIO DE INTERPRETACAO DE CODIGO**//
+//1-Ele vai incrementar valores a i,ate i ser menor que cinco
+//o resultado a ser exibido será 10
+
+//2-a) 19,21,23,25,27,30
+//b) Eu acredito que nao.
+
+//3- se nao tivesse com erro no prompt apresentaria 3 asteristicos
+
+console.log("olá mundo")
+
+const nome=prompt("Qual o seu nome")
