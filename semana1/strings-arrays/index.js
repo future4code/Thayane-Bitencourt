e5fb4ad70541d0e0d0f35ec40e7cced56b51b71e
Thayane-Bitencourt <thayanebitencourt.ssa@gmail.com>
2021-08-02 06:42:46 -0300

    atualizando

---
+++ semana1/strings-arrays/index.js
@@ -0,0 +1,67 @@
+
+//EXERCICIO DE INTERPRETAÇÂO//
+//PRIMEIRA QUESTAO
+// a. undefinid
+//b. null
+//c. 11 //espaço=lenght
+//d. 3
+//e. a posição 1 do array ao inves de ser 4, passa a ser 19, já que é um array de 11 posiçoes
+//f. o valor é 9, diferente do anterior, que troca valor
+
+//segunda questao
+// SUBI NUM ÔNIBUS EM MIRROCOS - ELE VAI COLOCAR EM CAIXA ALTA TODA
+//FRASE E SUBSTITUIR ONDE TEM A POR I
+
+//EXERCICIO DE ESCRITA DE CÓDIGO
+//1 QUESTAO
+//  const nomeDoUsuario = prompt("Qual seu nome?")
+//  const emailDoUsuario = prompt("Qual seu e-mail?")
+//  console.log("O e-mail " ,emailDoUsuario, "foi cadastrado com sucesso. Seja bem-vindo(a),",nomeDoUsuario,"!")
+
+//2  QUESTAO
+// 
+// let comidasFav =  ["lasanha", "feijoada", "escondidinho","serapatel","dobradinha"]
+// console.log(comidasFav)
+
+// let i = 0
+// while (i<5){
+//     console.log(comidasFav[i])
+//     i++
+// }
+
+// let comidaUsu = prompt("Qual sua comida favorita?")
+
+// {
+// comidasFav[1]=comidaUsu
+// }
+// console.log(comidasFav)
+
+//3 questao
+
+// //[1]
+
+// const tarefa1 = prompt("Qual primeira tarefa ira realizar")
+// const tarefa2 = prompt("Qual segunda tarefa ira realizar")
+// const tarefa3 = prompt("Qual terceira tareda ira realizar")
+// let listaTarefa = [tarefa1,tarefa2,tarefa3]
+// listaTarefa.forEach(listaTarefa=>{
+//  console.log(listaTarefa)
+
+// })
+
+// var i = prompt("Qual tarefa já realizou? 0- tarefa 1; 1 - tarefa 2 e 2- para tarefa 3")
+
+
+//  listaTarefa.splice(listaTarefa.indexOf(i,1));
+
+//  listaTarefa.forEach(listaTarefa=>{
+//      console.log(listaTarefa)
+   
+//     })
+
+// }
+//DESAFIOS
+//PRIMEIRO DESAFIO
+//SEGUNDO DESAFIO
+
+
