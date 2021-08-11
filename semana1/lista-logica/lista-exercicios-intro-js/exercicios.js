78cd77c1722f617d35d719a0e36582ec38fbb380
Thayane-Bitencourt <thayanebitencourt.ssa@gmail.com>
2021-08-10 21:43:34 -0300

    atualizando

--- semana1/lista-logica/lista-exercicios-intro-js/exercicios.js
+++ semana1/lista-logica/lista-exercicios-intro-js/exercicios.js
@@ -1,116 +1,125 @@
 // EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
 
 // EXERCÍCIO 0A
-function soma(num1, num2) {
-  // receber os números
-  prompt("Informe um numero", num1)
+ function soma(num1, num2) {
+// receber os números
+   prompt("Informe um numero", num1)
   prompt("Informe outro número", num2)
-  return num1 + num2
+   return num1 + num2
 
-}
+ }
 
 // EXERCÍCIO 0B
-function imprimeMensagem() {
-  //vai imprimir mensagem no console
-  const mensagem = prompt('Digite uma mensagem!')
+ function imprimeMensagem() {
+//vai imprimir mensagem no console
+   const mensagem = prompt('Digite uma mensagem!')
 
-  console.log(mensagem)
-}
+   console.log(mensagem)
+ }
 
 // EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
 
 // EXERCÍCIO 01
-function calculaAreaRetangulo() {
-  const altura = prompt("Informe a altura")
-  const base = prompt("Informe a base")
-  calculaAreaRetangulo = altura * base
-  console.log(calculaAreaRetangulo)
-}
+ function calculaAreaRetangulo(base,altura) {
+   const altura = prompt("Informe a altura")
+   const base = prompt("Informe a base")
+   calculaAreaRetangulo = altura * base
+   console.log(calculaAreaRetangulo)
+ }
 
 // EXERCÍCIO 02
-function imprimeIdade() {
-  const anoAtual = prompt("Informe ano atual")
-  const anoNascimento = prompt("Informe o ano de nascimento")
-  imprimeIdade = anoAtual - anoNascimento
+ function imprimeIdade() {
+   const anoAtual = prompt("Informe ano atual")
+   const anoNascimento = prompt("Informe o ano de nascimento")
+   imprimeIdade = anoAtual - anoNascimento
   console.log(imprimeIdade)
   
 
 }
 
 // EXERCÍCIO 03
-function calculaIMC(peso, altura) {
-  calculaIMC =  (peso/(altura*altura));
-  console.log(calculaIMC)
-  }
+ function calculaIMC(peso, altura) {
+   const valorAltura = altura * altura
+   calculaIMC =  peso /valorAltura
+   console.log(calculaIMC)
+   }
 
 // EXERCÍCIO 04
-function imprimeInformacoesUsuario() {
-const nome = prompt("Qual o seu nome?")
-const idade = prompt("Qual a sua idade?")
-const email = prompt("Qual seu email?")
-imprimeInformacoesUsuario(`Meu nome é:` ${nome}, `tenho`${idade}`anos,e o meu email é :`${email})
-}
+ function imprimeInformacoesUsuario() {
+ const nome = prompt("Qual o seu nome?")
+ const idade = prompt("Qual a sua idade?")
+ const email = prompt("Qual seu email?")
+ imprimeInformacoesUsuario(`Meu nome é:` ${nome}, `tenho`${idade}`anos,e o meu email é :`${email})
+ }
 
 // EXERCÍCIO 05
-function imprimeTresCoresFavoritas (cor1,cor2,cor3){
-const cor1 =prompt("Qual sua cor favorita 1? ")
-const cor2 =prompt("Qual sua cor favorita 2? ")
-const cor3 =prompt("Qual sua cor favorita 3? ")
+ function imprimeTresCoresFavoritas (cor1,cor2,cor3){
+ const cor1 =prompt("Qual sua cor favorita 1? ")
+ const cor2 =prompt("Qual sua cor favorita 2? ")
+ const cor3 =prompt("Qual sua cor favorita 3? ")
  console.log(imprimeCoresFavoritas())
 
-}
+ }
 
 // EXERCÍCIO 06
-function retornaStringEmMaiuscula(string) {
-const string = prompt("diga uma coisa qualquer")
-console.log(retornaStringEmMaiuscula.toUpperCase())
-}
+ function retornaStringEmMaiuscula(string) {
+ const string = prompt("diga uma coisa qualquer")
+ console.log(retornaStringEmMaiuscula.toUpperCase())
+ }
 
 // EXERCÍCIO 07
-function calculaIngressosEspetaculo(custo, valorIngresso) {
-  calculaIngressosEspetaculo= custo/valorIngresso
- console.log("O preço do ingresso precisa ser" + calculaIngressos())
+ function calculaIngressosEspetaculo(custo, valorIngresso) {
+   calculaIngressosEspetaculo= custo/valorIngresso
+  console.log("O preço do ingresso precisa ser" + calculaIngressos())
  )
 
-}
+ }
 
 // EXERCÍCIO 08
-function checaStringsMesmoTamanho(string1, string2) {
- console.log(checaStringsMesmoTamanho ((string1.lenght)===(string2.lenght))
+ function checaStringsMesmoTamanho(string1, string2) {
+  console.log(checaStringsMesmoTamanho ((string1.lenght)===(string2.lenght))
 
-}
+ }
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
-  console.log(array[0])
-}
+   console.log(array[0])
+ }
 
 // EXERCÍCIO 10
-function retornaUltimoElemento(array) {
-  const arrayPrimeiro = array[0]
-  const ultimoArray = [array.lenght -1]
-  console.log(arrayPrimeiro,ultimoArray)
+ function retornaUltimoElemento(array) {
+   const arrayPrimeiro = array[0]
+   const ultimoArray = [array.lenght -1]
+   console.log(arrayPrimeiro,ultimoArray)
 
-}
+ }
 
 // EXERCÍCIO 11
-function trocaPrimeiroEUltimo(array) {
-  const arrayPrimeiro = array[0]
-  const ultimoArray = [array.lenght -1]
-  ultimoArray<= arrayPrimeiro
+ function trocaPrimeiroEUltimo(array) {
+   const arrayPrimeiro = array[0]
+   const ultimoArray = [array.lenght -1]
+   ultimoArray<= arrayPrimeiro
 
-}
+ }
 
 // EXERCÍCIO 12
-function checaIgualdadeDesconsiderandoCase(string1, string2) {
-  // implemente sua lógica aqui
+function checaIgualdadeDesconsiderandoCase (string1, string2) {
+  (checaIgualdadeDesconsiderandoCase.toUpperCase());
+  
+  return string1>=string2
 
 }
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
-  // implemente sua lógica aqui
-
+  const anoAtual = prompt(Number("Informe o ano atual")
+  var anoNascimento = prompt(Number("Informe o ano de nascimento")
+  const anoEmissao = prompt(Number("Informe o ano de emissao da carteira")
+ 
+  checaRenovacaoRG = anoAtual-anoNascimento{
+ if(resultado<=20){
+   
+ }
 }
 
 // EXERCÍCIO 14
---
+++ semana2/objetos/index.js
@@ -0,0 +1,38 @@
+//Exercicio de interpretacao
+//primeira questão
+//letra a
+//Matheus Nachtergaele
+//Virginia Cavendish
+//canal:"Globo",horario:"14h"
+
+//segunda questao
+
+//letra a
+// nome:Juca,idade: 3, raca:SRD
+//nome:Juba,idade:3, raca:SRD
+//nome:Jubo,idade:3, raca:SRD
+
+//letra b 
+//vai repetir o objeto anterior
+
+//terceira questao
+
+//letra a
+//false
+//undefined
+
+//letra b
+//falso pois o backender no objeto é falso
+//undefined pois altura nao foi declarada
+
+//EXERCICIO DE CODIGO
+
+// function minhaFuncao(objeto, propriedade) {
+// 	return objeto[propriedade]
+// }
+
+// const pessoa = {
+//   nome: "Amanda", 
+//   apelido: ["Amandinha,Mandinha,Mandi"]}
+
+// console.log("Eu sou", pessoa.nome,"mas podem me chamar de: ",pessoa.apelido)
