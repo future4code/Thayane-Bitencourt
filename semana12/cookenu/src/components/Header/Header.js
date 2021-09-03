1c0086dbf8b070750fb7339a61ccf9eed4465371
Thayane-Bitencourt <thayanebitencourt.ssa@gmail.com>
2021-09-03 20:45:14 -0300

    atual

---
+++ semana12/cookenu/src/components/Header/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import {StyledToolbar} from "./styled"
+import Button from '@material-ui/core/Button'
+import {goToRecipesList, goToLogin} from "../../routes/coordinator"
+import {useHistory} from "react-router-dom"
+
+const Header = ({rightButtonText, setRightButtonText}) => {
+    const token = localStorage.getItem("token")
+    const history = useHistory()
+
+    const logout = () => {
+        localStorage.removeItem("token")
+    }
+
+    const rightButtonAction = () => {
+        if (token){
+            logout()
+            setRightButtonText("Login")
+            goToLogin(history)
+        } else {
+            goToLogin(history)
+        }
+    }
+
+    return (
+        <AppBar position="static">
+            <StyledToolbar>
+                <Button onClick={() => goToRecipesList(history)} color="inherit">Cookenu</Button>
+                <Button onClick={rightButtonAction} color="inherit">{rightButtonText}</Button>
+            </StyledToolbar>
+        </AppBar>
+    )
+}
+
+export default Header
