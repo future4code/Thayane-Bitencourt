fd0abc5ab6177fcf7ae7f9fff560270841475bf4
Thayane-Bitencourt <thayanebitencourt.ssa@gmail.com>
2021-09-30 21:35:51 -0300

    terminando exercicio

---
+++ semana15/aula41/index.js
@@ -0,0 +1,66 @@
+
+/*Exercicio 1a*/
+//utilizamos o process.argv, que vai funcionar como array
+
+/*Exercicio 1, b e c*///var input = process.argv[2]
+
+//var idade = Number(process.argv[3])
+
+//const soma = (idade) => {return idade +7}
+
+
+//console.log(`Olá ${input} ! Você tem ${idade} anos. Em sete anos, você terá ${soma(idade)}`)
+
+
+ /*Exercicio 2*/
+
+//  const operacao = (process.argv[2])
+//  var num1 = Number((process.argv[3]))
+//  var num2 = Number((process.argv[4]))
+
+//  var resultado
+
+//  switch(operacao){
+//      case"soma":
+//      resultado = num1+num2;
+//      break;
+//      case"sub":
+//      resultado = num1-num2;
+//      break;
+//      case"mult":
+//      resultado = num1*num2;
+//      break;
+//       case"div":
+//      resultado = num1/num2;
+//      break;
+
+//  }
+
+//  console.log(resultado)
+
+/*exercicio3*/
+
+// var tarefa = (process.argv[2])
+// var tarefa1 = (process.argv[3])
+// var tarefa2 = (process.argv[4])
+// var tarefa3 = (process.argv[5])
+// var tarefa4 = (process.argv[6])
+
+// console.log("Lista de tarefa:")
+// console.table(tarefa)
+// console.table(tarefa1)
+// console.table(tarefa2)
+// console.table(tarefa3)
+// console.table(tarefa4)
+
+/*Exercicio 3/ let list = []
+for (i=0; process.argv.length>i; i++){
+ list.push(process.argv[i])
+
+
+}
+
+list.forEach(p=>{
+
+    console.log(p)
+}
